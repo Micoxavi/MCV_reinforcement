d1066e3de9addb89dc17f1ef22a86c13782a3554
Xavier Mico <xavier.mico.perez@gmail.com>
2024-03-28 16:41:43 +0100

    Changed input_size to input_shape

--- src/model.py
+++ src/model.py
@@ -15,7 +15,7 @@ class DQN(nn.Module):
 
     Atributes
     ---------
-        input_size: Input image's size
+        input_shape: Input image's size
         nb_actions: number of action-value to output. Pong has 6 actions by default.
 
     Methods
@@ -24,18 +24,18 @@ class DQN(nn.Module):
         Defines the forward poass of the neural network.
 
     """
-    def __init__(self, input_size: tuple, nb_actions: int) -> None:
+    def __init__(self, input_shape: tuple, nb_actions: int) -> None:
         """
         Initializes the Deep Q-learning Network.
         :Parameters:
-            input_size: Input image's size
+            input_shape: Input image's size
             nb_actions: number of action-value to output. Pong has 6 actions by default.
         """
         super(DQN, self).__init__()
-        self.conv1 = nn.Conv2d(input_size[0], 32, kernel_size=8, stride=4)
+        self.conv1 = nn.Conv2d(input_shape[0], 32, kernel_size=8, stride=4)
         self.conv2 = nn.Conv2d(32, 64, kernel_size=4, stride=2)
         self.conv3 = nn.Conv2d(64, 64, kernel_size=3, stride=1)
-        self.fc4 = nn.Linear(input_size[1] * input_size[2] * 64, 512)
+        self.fc4 = nn.Linear(input_shape[1] * input_shape[2] * 64, 512)
         self.fc5 = nn.Linear(512, nb_actions)
 
     def forward(self, x: torch.tensor) -> torch.tensor:
