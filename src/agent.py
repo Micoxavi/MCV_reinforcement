761f354b8b526f0f38bc4cf3045d0d32c3de8744
Xavier Mico <xavier.mico.perez@gmail.com>
2024-03-29 23:25:30 +0100

    Implemented agent initialization, select_actions function and train function

---
+++ src/agent.py
@@ -0,0 +1,141 @@
+"""
+This script contains a DQNAgent class
+"""
+
+import torch
+from torch import nn
+from torch import optim
+from torch.nn import functional as F
+import numpy as np
+
+from model import DQN
+
+
+class DQNAgent:
+    """
+    The DQNAgent agent class. Implements the Q-learning algorithm 
+    for reinforcement learning tasks.
+
+    Atributes
+    ---------
+        input_shape: Input image's shape.
+        nb_actions: number of action-value to output. Pong has 6 actions by default.
+        device: cuda when gpu available, cpu otherwise.
+        params: Dictionary with the different hyperparameters configuration.
+
+    Methods
+    -------
+    select_actions(state)
+        Selects actions based on the epsilon-greedy method and the current state.
+
+    """
+
+    def __init__(self, input_shape: tuple, nb_actions: int, device: str, params: dict,
+                 env: any) -> None:
+        """
+        DQNAgent initialization
+
+        :Parameters:
+            input_shape: Input image's shape.
+            nb_actions: number of action-value to output. Pong has 6 actions by default.
+            device: cuda when gpu available, cpu otherwise.
+            params: Dictionary with the different hyperparameters configuration.
+            env: Game enviroment
+
+        """
+        self.device = device
+        self.params = params
+        self.enviroment = env
+
+        # @train_network -> Used for action selection during the agent's interaction with
+        # the enviroment. Its parameters are updated based on the TD-error calculated during
+        # training.
+        self.train_network = DQN(input_shape, nb_actions).to(self.device)
+
+        # @target_network -> used for estimating the target Q-values during the training process
+        # its parameters are are periodically updated by copying the parameters from the
+        # train_network, helping to stabilize the training process.
+        self.target_network = DQN(input_shape, nb_actions).to(self.device)
+        self.target_network.load_state_dict(self.train_network.state_dict())
+
+        self.optimizer = optim.Adam(self.train_network.parameters(), lr=self.params['lr'])
+
+    def select_actions(self, state: any) -> int:
+        """
+        Function used to select actions based on the epsilon-greedy method.
+        When the epsilon is bigger than a random number, the action is taken randomly
+        and the agent only observes. When epsilon is smaller, the agent decides the action.
+
+        :Parameters:
+            state: Actual bar position on the image.
+
+        """
+        if np.random.rand() <= self.params['eps']:
+            action = self.enviroment.action_Space.sample()
+
+        else:
+            with torch.no_grad():
+                input_state = torch.FloatTensor(np.array(state)).unsqueeze(0).to(self.device)
+
+                # pylint: disable=not-callable
+                action = int(self.train_network(input_state).forward().max(1)[1])
+                # pylint: enable=not-callable
+
+        return action
+
+    def train(self, replay_buffer, batch_size: int, discount):
+        """
+        Agent training function. As every agent can be reused for different tasks, the training
+        function is usually encapsulated within the agent itself for easier modularity.
+
+        :Parameters:
+            replay_buffer: Memory buffer with tuples (state, next_state, action, reward, done)
+            batch_size: 
+            discount:         
+        """
+
+        state, next_state, action, reward, done = replay_buffer(batch_size)
+
+        state_batch = torch.FloatTensor(state).to(self.device)
+        next_state_batch = torch.FloatTensor(next_state).to(self.device)
+        action_batch = torch.LongTensor(action).to(self.device)
+        reward_batch = torch.FoatTensor(reward).to(self.device)
+        done_batch = torch.FloatTensor(1. - done).to(self.device)
+
+        # Get train Q network values for the current state and action.
+
+        # pylint: disable=not-callable
+        train_values = self.train_network(state_batch).gather(1, action_batch)
+
+        # Get the target Q network values.
+        # @(reward_batch + done_batch * discount) --> immediate reward + discount
+        # @(torch.max(self.target_network(next_state_batch).detach()) --> next_states Q values
+
+        with torch.no_grad():
+
+            target_values = reward_batch + done_batch * discount * \
+                             torch.max(self.target_network(next_state_batch).detach(),
+                                       dim=1)[0].view(batch_size, -1)
+        # pylint: enable=not-callable
+
+        # Get the loss between train Q network and target Q network. DQN typically uses either
+        # Huber loss, or MSE loss. @smooth_l1_loss --> similar to Huber.
+
+        loss = F.smooth_l1_loss(train_values, target_values)
+
+        # Optimize the parameters with the loss
+
+        # Zero out the gradient to prevent errors from accumulation to previous values. 
+        self.optimizer.zero_grad()
+        loss.backward()
+
+        # Apply gradient clipping to avoid exploding gradients. 
+        # @clamp_ --> Operation done in-place modifying original gradient data.
+        for param in self.train_network.parameters():
+            param.grad.data.clamp_(-1, 1)
+
+        self.optimizer.step()
+
+        # return the loss to monitor it.
+        return loss.detach().cpu().numpy()
+
